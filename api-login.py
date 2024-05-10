3cbc4c01f8d106be1a9bfbfe56d8ab7fe4b201fd
BuildTools <unconfigured@null.spigotmc.org>
2024-05-10 14:07:32 +0200

    Modificaciones a la estructura de api-login, falta conocer la estructura de datos para guardarlos todos.

--- api-login.py
+++ api-login.py
@@ -7,14 +7,31 @@ app_users.config['JWT_SECRET_KEY'] = 'easy_gest238973'
 jwt = JWTManager(app_users)
 
 users = {}
-data = {}
+organizers = {}
+assistants = {}
+events = {}
 
+# cargar y guardar datos en la BD
 def load():
     pass
 
 def save():
     pass
 
+# funciones get, post, put y delete
+@app_users.route('/data', methods=['GET'])
+@jwt_required()
+
+@app_users.route('/data', methods=['POST'])
+@jwt_required()
+
+@app_users.route('/data', methods=['PUT'])
+@jwt_required()
+
+@app_users.route('/data', methods=['DELETE'])
+@jwt_required()
+
+# funciones de registro/inicio de sesión
 @app_users.route('/signup', methods=['POST'])
 def signup():
     user = request.args.get('user', '')
@@ -25,6 +42,7 @@ def signup():
         hashed = hashlib.sha256(password.encode()).hexdigest()
         users[user] = hashed
         return f'Usuario {user} registrado', 200
+
 
 if __name__ == '__main__':
     app_users.run(debug=True)
