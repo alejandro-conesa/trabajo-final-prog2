08a69842e6d47328b7e42653dfab7e22049baa9a
alejandro-conesa <acm382@alu.ua.es>
2024-05-23 00:57:05 +0200

    -

--- data-management.py
+++ data-management.py
@@ -20,18 +20,18 @@ def save_events(event_obj_list: list) -> None:
                         event['final_hour'], event['price'], event['organizer'])
         sentence = f'INSERT INTO Events VALUES {event_values}'
         cursor.execute(sentence)
-        for assistant in event['assistants']:
-            assist_values = (assistant, event['event_id'])
+        for assistant1 in event['assistants']:
+            assist_values = (assistant1, event['event_id'])
             sentence = f'INSERT INTO Assist VALUES {assist_values}'
             cursor.execute(sentence)
     connection.close()
 
 
 def load_events() -> list:
-    def evlappend(el, row):
-        el.append({'event_id': row[0], 'tipo': row[1], 'nombre': row[2], 'mas18': row[3], 'start_date': row[4],
-                   'final_date': row[5], 'assistant_number': row[6], 'location': row[7], 'start_hour': row[8],
-                   'final_hour': row[9], 'price': row[10], 'organizer': row[11],  'assistant_list': [row[12]]})
+    def evlappend(el, row1):
+        el.append({'event_id': row1[0], 'tipo': row1[1], 'nombre': row1[2], 'mas18': row1[3], 'start_date': row1[4],
+                   'final_date': row1[5], 'assistant_number': row1[6], 'location': row1[7], 'start_hour': row1[8],
+                   'final_hour': row1[9], 'price': row1[10], 'organizer': row1[11],  'assistant_list': [row1[12]]})
         return el
 
     connection = sqlite3.connect('ProyectoP2.db')
