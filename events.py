2c67acd2aae2828eebb7f077714c750746518ed0
imd24 <ignacioenriquemd@gmail.com>
2024-05-22 21:46:00 +0200

    Este código define una clase Evento para gestionar eventos. Incluye información como el tipo, nombre, fechas, ubicación, precio y organizador del evento. La clase tiene una lista de asistentes que puede ser modificada para añadir o quitar asistentes. También tiene una función que devuelve un diccionario con todos los detalles del evento, incluyendo el número de asistentes calculado automáticamente.

---
+++ events.py
@@ -0,0 +1,44 @@
+from user import User
+from assistant import Assistant
+
+class Evento:
+    def __init__(self, event_id: int, tipo: str, nombre: str, mas18: int, start_date: str, final_date: str,
+                 assistant_number: str, location: str, start_hour: str, final_hour: str, price: str,
+                 organizer: str):
+        self._event_id = event_id
+        self._tipo = tipo
+        self._nombre = nombre
+        self._mas18 = mas18
+        self._start_date = start_date
+        self._final_date = final_date
+        self._assistant_number = assistant_number
+        self._location = location
+        self._start_hour = start_hour
+        self._final_hour = final_hour
+        self._price = price
+        self._organizer = organizer
+        self._asistentes = []
+
+    def return_dict(self) -> dict:
+        info_dict = {
+            'event_id': self._event_id,
+            'tipo': self._tipo,
+            'nombre': self._nombre,
+            'mas18': self._mas18,
+            'start_date': self._start_date,
+            'final_date': self._final_date,
+            'assistant_number': self._assistant_number,
+            'location': self._location,
+            'start_hour': self._start_hour,
+            'final_hour': self._final_hour,
+            'price': self._price,
+            'organizer': self._organizer,
+            'asistentes': [asistente.return_dict() for asistente in self._asistentes]
+        }
+        return info_dict
+
+    def modificar_asistentes(self, action: str, asistente: Assistant):
+        if action == 'añadir' and asistente not in self._asistentes:
+            self._asistentes.append(asistente)
+        elif action == 'quitar' and asistente in self._asistentes:
+            self._asistentes.remove(asistente)
