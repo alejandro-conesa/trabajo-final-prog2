81f9e8d91a93769cd546ea4ed5be7720a3f676ab
alejandro-conesa <acm382@alu.ua.es>
2024-05-23 00:30:54 +0200

    Cambio en la estructura de manejo de datos, no hace falta cargar cada vez la informacion de los participantes u organizadores, solo los de los eventos.

--- data-management.py
+++ data-management.py
@@ -9,70 +9,44 @@ def obj_to_dict(obj_list: list) -> list:
         dict_list.append(obj.return_dict())
     return dict_list
 
-
-def assistant_dict_to_obj(dict_list: list) -> list:
-    obj_list = []
-    for di in dict_list:
-        obj = assistant.Assistant(username=di['username'], password=di['password'], email=di['email'])
-        obj.register(name=di['name'], birth_date=di['birth_date'], id_num=di['id_num'], tlf=di['tlf'],
-                     address=di['address'])
-        obj.modify_event_list(event_list=di['event_list'])
-        obj_list.append(obj)
-    return obj_list
-
-
-def organizer_dict_to_obj(dict_list: list) -> list:
-    obj_list = []
-    for di in dict_list:
-        obj = organizer.Organizer(username=di['username'], password=di['password'], email=di['email'])
-        obj.register(type_org=di['type_org'], name=di['name'], id_user=di['id_user'])
-        obj.modify_event_list(event_list=di['event_list'])
-        obj_list.append(obj)
-    return obj_list
-
-
-def save(org_list: list, assist_list: list) -> None:
-    org_dict_list = obj_to_dict(org_list)
-    assist_dict_list = obj_to_dict(assist_list)
+def save_events(event_obj_list: list) -> None:
     connection = sqlite3.connect('ProyectoP2.db')
     cursor = connection.cursor()
-    for org in org_dict_list:
-        tupla_org = (org['username'], org['name'], org['id_user'], org['type_org'])
-        tupla_usr = (org['username'], org['password'], org['email'])
-        sentence_usr = f'INSERT INTO Users VALUES {tupla_usr}'
-        sentence_org = f'INSERT INTO Organizers VALUES {tupla_org}'
-        try:
-            cursor.execute(sentence_usr)
-            cursor.execute(sentence_org)
-        except sqlite3.IntegrityError:
-            pass
-    for assist in assist_dict_list:
-        tupla_assist = (assist['username'], assist['name'], assist['id_num'], assist['birth_date'],
-                        assist['tlf'], assist['address']['city'], assist['address']['code'], assist['address']['street'])
-        tupla_usr = (assist['username'], assist['password'], assist['email'])
-        sentence_usr = f'INSERT INTO Users VALUES {tupla_usr}'
-        sentence_assist = f'INSERT INTO Assistants VALUES {tupla_assist}'
-        try:
-            cursor.execute(sentence_usr)
-            cursor.execute(sentence_assist)
-        except sqlite3.IntegrityError:
-            pass
+    event_list = obj_to_dict(event_obj_list)
+    for event in event_list:
+        event_values = (event['event_id'], event['tipo'], event['nombre'], event['mas18'], event['start_date'],
+                        event['final_date'], event['assistant_number'], event['location'], event['start_hour'],
+                        event['final_hour'], event['price'], event['organizer'])
+        sentence = f'INSERT INTO Events VALUES {event_values}'
+        cursor.execute(sentence)
+        for assistant in event['assistants']:
+            assist_values = (assistant, event['event_id'])
+            sentence = f'INSERT INTO Assist VALUES {assist_values}'
+            cursor.execute(sentence)
     connection.close()
 
 
-def load(alr_org_list: list, alr_assist_list: list) -> list:
-    # obtener de la db una lista de organizers y de asistentes
+def load_events() -> list:
+    def evlappend(el, row):
+        el.append({'event_id': row[0], 'tipo': row[1], 'nombre': row[2], 'mas18': row[3], 'start_date': row[4],
+                   'final_date': row[5], 'assistant_number': row[6], 'location': row[7], 'start_hour': row[8],
+                   'final_hour': row[9], 'price': row[10], 'organizer': row[11],  'assistant_list': [row[12]]})
+        return el
+
     connection = sqlite3.connect('ProyectoP2.db')
     cursor = connection.cursor()
-    sentence_assist = f'SELECT * FROM Users LEFT JOIN Assistants ON Users.username = Assistants.username'
-    sentence_org = f'SELECT * FROM Users LEFT JOIN Organizers ON Users.username = Organizers.username'
-    cursor.execute(sentence_assist)
+    sentence = 'SELECT * FROM Events LEFT JOIN Assist ON Events.event_id = Assist.event_id'
+    cursor.execute(sentence)
+    event_list = []
     query = cursor.fetchall()
+    i = 0
     for row in query:
-        if row[0] in alr_assist_list:
-
-    org_list = organizer_dict_to_obj()
-    assist_list = assistant_dict_to_obj()
-    return [org_list, assist_list]
-
-
+        if i == 0:
+            i += 1
+            event_list = evlappend(event_list, row)
+        elif row[0] != event_list[i-1]['event_id']:
+            event_list = evlappend(event_list, row)
+            i += 1
+        elif row[0] == event_list[i-1]['event_id']:
+            event_list[i-1]['assistant_list'].append(row[12])
+    return event_list
